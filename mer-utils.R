610036edb24277893a3ec4d71a75777b17cbccf9
Austin F. Frank <austin.frank@gmail.com>
2011-02-24 15:17:09 -0500

    * mer-utils.R:   add maxcorr.mer

--- mer-utils.R
+++ mer-utils.R
@@ -19,23 +19,23 @@ vif.mer <- function (fit) {
 
 kappa.mer <- function (fit,
                        scale = TRUE, center = FALSE,
-                       add.intercept = TRUE) {
+                       add.intercept = TRUE,
+                       exact = FALSE) {
     X <- fit@X
     nam <- names(fixef(fit))
     
     ## exclude intercepts
-    nrp <- sum(1 * (nam == "Intercept" | nam == "(Intercept)"))
+    nrp <- sum(1 * (nam == "(Intercept)"))
     if (nrp > 0) {
-        X <- X[-(1:nrp), -(1:nrp), drop = FALSE]
+        X <- X[, -(1:nrp), drop = FALSE]
         nam <- nam[-(1:nrp)]
     }
 
     if (add.intercept) {
-        X <- cbind(1, scale(X, scale = scale, center = center))
-        colnames(X)[1] <- "(Intercept)"
-        kappa(X)
+        X <- cbind(rep(1), scale(X, scale = scale, center = center))
+        kappa(X, exact = exact)
     } else {
-        kappa(scale(X, scale = scale, center = scale))
+        kappa(scale(X, scale = scale, center = scale), exact = exact)
     }
 }
 
@@ -49,13 +49,13 @@ colldiag.mer <- function (fit,
     result <- NULL
     if (center) 
         add.intercept <- FALSE
-    if (is.matrix(mod) || is.data.frame(mod)) {
-        X <- as.matrix(mod)
-        nms <- colnames(mod)
+    if (is.matrix(fit) || is.data.frame(fit)) {
+        X <- as.matrix(fit)
+        nms <- colnames(fit)
     }
-    else if (class(mod) == "mer") {
-        nms <- names(fixef(mod))
-        X <- mod@X
+    else if (class(fit) == "mer") {
+        nms <- names(fixef(fit))
+        X <- fit@X
         if (any(grepl("(Intercept)", nms))) {
             add.intercept <- FALSE
         }
@@ -89,4 +89,26 @@ colldiag.mer <- function (fit,
 
     result <- data.frame(cbind(condindx, pi))
     zapsmall(result)
+}
+
+maxcorr.mer <- function (fit,
+                         exclude.intercept = TRUE) {
+    so <- summary(fit)
+    corF <- so@vcov@factors$correlation
+    nam <- names(fixef(fit))
+
+    ## exclude intercepts
+    ns <- sum(1 * (nam == "Intercept" | nam == "(Intercept)"))
+    if (ns > 0 & exclude.intercept) {
+        corF <- corF[-(1:ns), -(1:ns), drop = FALSE]
+        nam <- nam[-(1:ns)]
+    }
+    corF[!lower.tri(corF)] <- 0
+    maxCor <- max(corF)
+    minCor <- min(corF)
+    if (abs(maxCor) > abs(minCor)) {
+        zapsmall(maxCor)
+    } else {
+        zapsmall(minCor)
+    }
 }
